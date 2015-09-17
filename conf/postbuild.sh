989f17a125f83b1905ace4bc232ace5efe9c80de
Hugues Lepesant <hugues@lepesant.com>
2015-09-17 22:53:45 +0200

    wheezy to jessie

--- conf/postbuild.sh
+++ conf/postbuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#if [ -f "/tmp/pwd.ldif" ]
-#then
-#	/usr/bin/ldapmodify -Y EXTERNAL -H ldapi:/// -f /tmp/pwd.ldif
-#fi
+if [ -f "/tmp/pwd.ldif" ]
+then
+	/usr/bin/ldapmodify -Y EXTERNAL -H ldapi:/// -f /tmp/pwd.ldif
+fi
 
 if [ -f "/tmp/olcLog.ldif" ]
 then
@@ -20,5 +20,5 @@ fi
 
 if [ -f "/tmp/zacacia.ldif" ]
 then
-	/usr/bin/ldapadd -x -D  cn=admin,dc=zarafa,dc=com -w password -f /tmp/zacacia.ldif
+	/usr/bin/ldapadd -x -D cn=admin,dc=zarafa,dc=com -w password -f /tmp/zacacia.ldif
 fi
